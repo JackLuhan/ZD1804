5f22b909fa265696070132adba80582f05a64ac7
鲁寒 <1317406121@qq.com>
2019-10-11 09:38:38 +0800

    我要提交代码--第一次

---
+++ day02/01/01.js
@@ -0,0 +1,4 @@
+// document.write("中电1804都是好学生，爱学习的学生")
+// <script type="text/javascript"></script>
+alert("中电1804都是好学生")
+// alert(1231213121)
---
+++ day05/homework/珠穆拉玛峰.js
@@ -0,0 +1,3 @@
+document.write("珠穆拉玛峰是世界第一高山峰,约高为：")
+document.write(8844)
+document.write("米")
